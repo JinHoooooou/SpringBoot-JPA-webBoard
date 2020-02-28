5061cbde92f819ba7a360dad2054dcb6d895e4a9
JinHoooooou <jinho4744@naver.com>
2020-02-28 16:24:09 +0900

    second-cycle-2-add-print-user-list-Add print user list method

--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -1,14 +1,26 @@
 package net.slipp.web;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
+  List<UserDto> userList = new ArrayList<>();
+
   @PostMapping("/signUpUser")
   public String signUpUser(UserDto user, Model model){
     System.out.println(user);
-    return "signUpUser";
+    userList.add(user);
+    return "redirect:/userList";
+  }
+
+  @GetMapping("/userList")
+  public String printUserList(Model model){
+    model.addAttribute("userList", userList);
+    return "printUserList";
   }
 }
