2e4b150068995ef939b4dc3e84c69363560500bb
JinHoooooou <jinho4744@naver.com>
2020-03-11 00:32:00 +0900

    third-cycle-3-apply-new-html-and-refactoring-url-Refactor UserController url

--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -9,21 +9,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 
   @Autowired
   UserRepository userRepository;
 
-  @PostMapping("/signUpUser")
+  @PostMapping("")
   public String signUpUser(User user, Model model){
     System.out.println(user);
     userRepository.save(user);
-    return "redirect:/userList";
+    return "redirect:/users";
   }
 
-  @GetMapping("/userList")
+  @GetMapping("")
   public String printUserList(Model model){
     model.addAttribute("userList", userRepository.findAll());
     return "/user/userList";
