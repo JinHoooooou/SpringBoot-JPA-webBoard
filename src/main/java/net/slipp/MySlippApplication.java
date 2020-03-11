9314cce59f936618fc56d3231303746f04aa6282
JinHoooooou <jinho4744@naver.com>
2020-03-11 16:19:39 +0900

    third-cycle-4-add-update-user-info-Add 'HiddenHttpMethodFilter' bean in MySlippApplication to use PUT method

--- src/main/java/net/slipp/MySlippApplication.java
+++ src/main/java/net/slipp/MySlippApplication.java
@@ -2,6 +2,8 @@ package net.slipp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @SpringBootApplication
 public class MySlippApplication {
@@ -10,4 +12,8 @@ public class MySlippApplication {
     SpringApplication.run(MySlippApplication.class, args);
   }
 
+  @Bean
+  public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+    return new HiddenHttpMethodFilter();
+  }
 }
