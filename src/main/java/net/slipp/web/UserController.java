913dc2af62c5accfd3cf29eed32d3f7885b6487c
JinHoooooou <jinho4744@naver.com>
2020-03-11 15:13:43 +0900

    third-cycle-4-add-update-user-info-Add method about updating user info

--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -21,10 +22,22 @@ public class UserController {
     return "/user/signUpForm";
   }
 
+  @GetMapping("{id}/update")
+  public String goToUpdateFormPage(@PathVariable Long id, Model model) {
+    model.addAttribute("user", userRepository.findById(id).get());
+    return "/user/updateForm";
+  }
+
   @PostMapping("")
   public String signUpUser(User user, Model model){
     System.out.println(user);
     userRepository.save(user);
+    return "redirect:/users";
+  }
+
+  @PostMapping("{id}/update")
+  public String updateUser(@PathVariable Long id, User updateUser) {
+    userRepository.save(updateUser);
     return "redirect:/users";
   }
 
