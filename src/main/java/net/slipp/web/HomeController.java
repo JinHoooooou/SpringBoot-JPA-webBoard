d891a64cb6fc0701c9a7f1dc8d89021e15ad8186
JinHoooooou <jinho4744@naver.com>
2020-03-11 14:18:22 +0900

    third-cycle-3-apply-new-html-and-refactoring-url-Move index.html from static to templates directory

---
+++ src/main/java/net/slipp/web/HomeController.java
@@ -0,0 +1,13 @@
+package net.slipp.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+  @GetMapping("")
+  public String goToMainPage() {
+    return "index";
+  }
+
+}
