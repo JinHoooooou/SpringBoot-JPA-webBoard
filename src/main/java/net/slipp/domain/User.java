d4d00eb58534dc9438e8c713f455f9df9b89f3d5
JinHoooooou <jinho4744@naver.com>
2020-03-15 19:32:54 +0900

    Add strategy attribute in @GeneratedValue

--- src/main/java/net/slipp/domain/User.java
+++ src/main/java/net/slipp/domain/User.java
@@ -3,6 +3,7 @@ package net.slipp.domain;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,7 +15,7 @@ import lombok.ToString;
 @Entity
 public class User {
   @Id
-  @GeneratedValue
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   @Column(nullable = false, length = 20)
