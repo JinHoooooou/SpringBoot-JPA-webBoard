82110dbadb77c6e8ceba629e1d08cad2f414876c
JinHoooooou <jinho4744@naver.com>
2020-03-18 18:55:07 +0900

    fourth-cycle-5-2-add-printing-question-list-function-Fix HomeController to print created question list

--- src/main/java/net/slipp/web/HomeController.java
+++ src/main/java/net/slipp/web/HomeController.java
@@ -1,12 +1,18 @@
 package net.slipp.web;
 
+import net.slipp.domain.QuestionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+  @Autowired
+  private QuestionRepository questionRepository;
   @GetMapping("")
-  public String goToMainPage() {
+  public String goToMainPage(Model model) {
+    model.addAttribute("questionList", questionRepository.findAll());
     return "index";
   }
 
