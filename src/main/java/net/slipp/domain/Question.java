f8fdaca6bf7e657060136c0c5b74f86c9139777f
JinHoooooou <jinho4744@naver.com>
2020-03-27 19:00:55 +0900

    fifth-cycle-3-add-update-delete-question-function
    modify update url in show.html
    add goToUpdateQuestionForm method in Controller
    add updateQuestionForm.html
    apply header, footer, navigation to updateQuestionForm.html
    apply mustache template engine to updateQuestionForm.html
    add updateQuestion method in Controller
    modify delete url in show.html
    add deleteQuestion method in Controller

--- src/main/java/net/slipp/domain/Question.java
+++ src/main/java/net/slipp/domain/Question.java
@@ -43,4 +43,9 @@ public class Question {
     this.createDate = LocalDateTime.now()
         .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
   }
+
+  public void update(String title, String contents) {
+    this.title = title;
+    this.contents = contents;
+  }
 }
--- src/main/java/net/slipp/web/QuestionController.java
+++ src/main/java/net/slipp/web/QuestionController.java
@@ -7,9 +7,11 @@ import net.slipp.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -34,6 +36,12 @@ public class QuestionController {
     return "qna/show";
   }
 
+  @GetMapping("{id}/update")
+  public String goToUpdateQuestionFormPage(@PathVariable Long id, Model model) {
+    model.addAttribute("question", questionRepository.findById(id).get());
+    return "qna/updateQuestionForm";
+  }
+
   @PostMapping("")
   public String createQuestion(String title, String contents, HttpSession session) {
     if (!HttpSessionUtils.isLoginUser(session)) {
@@ -47,5 +55,17 @@ public class QuestionController {
     return "redirect:/";
   }
 
+  @PutMapping("{id}")
+  public String updateQuestion(@PathVariable Long id, String title, String contents) {
+    Question question = questionRepository.findById(id).get();
+    question.update(title, contents);
+    questionRepository.save(question);
+    return String.format("redirect:/questions/%d", id);
+  }
 
+  @DeleteMapping("{id}")
+  public String deleteQuestion(@PathVariable Long id) {
+    questionRepository.deleteById(id);
+    return "redirect:/";
+  }
 }
